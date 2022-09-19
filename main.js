799616421b2590122a57d63527f3a36a2b6daa34
mylennabra <mylenna.bra@gmail.com>
2022-09-19 15:04:14 -0300

    Apresentação de lista e estrutura de cadastro

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function addHouse() {
     
     let operation = document.querySelector('input[name="acao-imovel"]:checked').labels[0].outerText;
     let tipo = document.querySelector('select[name=tipo]').options[document.querySelector('select[name=tipo]').selectedIndex].text;
-    let state = document.querySelector('select[name=state]').options[document.querySelector('select[name=state]').selectedIndex].text;
+    let state = document.querySelector('select[name=state]').options[document.querySelector('select[name=state]').selectedIndex].value;
     let city = document.querySelector('select[name=city]').options[document.querySelector('select[name=city]').selectedIndex].text;
     let price = document.querySelector('input[name=price]').value;
 
@@ -10,8 +10,8 @@ function addHouse() {
     tabela.innerHTML += `
        <tr class="house" id="${index() + 1}">
             <td>${tipo}</td>
-            <td>${state}</td>
             <td>${city}</td>
+            <td>${state}</td>
             <td>${operation}</td>
             <td>${price}</td>
             <td style="cursor:pointer; " onclick="deleteHouse(${index() + 1})">x</td>
@@ -46,19 +46,17 @@ function addHouse() {
       })
       
     }
-    
 
 
-
-  function index() {
+function index() {
     let tabela = document.querySelectorAll(".house");
     return tabela.length;
-  }
+}
 
-  function eraseFields() {
+function eraseFields() {
     document.querySelector("form#house").reset()
-  }
+}
 
-  function deleteHouse(house) {
+function deleteHouse(house) {
     document.getElementById(house).remove();
-  }
+}
