4a708ec92e7525107f55e130ea310d3ea6522c15
mylennabra <mylenna.bra@gmail.com>
2022-09-21 23:26:37 -0300

    Lista de busca

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ function addHouse() {
     let city = document.querySelector('select[name=city]').options[document.querySelector('select[name=city]').selectedIndex].text;
     let price = document.querySelector('input[name=price]').value;
 
-    let tabela = document.getElementById("lista");
+    let tabela = document.getElementById("lista_cadastro");
+
+    // if (index() == 0){
+        
+    // }
+
     tabela.innerHTML += `
        <tr class="house" id="${index() + 1}">
-            <td>${tipo}</td>
-            <td>${city}</td>
-            <td>${state}</td>
-            <td>${operation}</td>
-            <td>${price}</td>
+            <td id="h-${index() + 1}-child-1">${tipo}</td>
+            <td id="h-${index() + 1}-child-2">${city}</td>
+            <td id="h-${index() + 1}-child-3">${state}</td>
+            <td id="h-${index() + 1}-child-4">${operation}</td>
+            <td id="h-${index() + 1}-child-5">${price}</td>
             <td style="cursor:pointer; " onclick="deleteHouse(${index() + 1})">x</td>
        </tr>
     `;
@@ -47,6 +52,37 @@ function addHouse() {
       
     }
 
+
+function searchHouse() {
+    let aux = index();
+
+    let keyword = document.querySelector('select[name=busca_tipo]').options[document.querySelector('select[name=busca_tipo]').selectedIndex].text;
+   
+    console.log(aux);
+    console.log(keyword);
+   
+   let temp = 1;
+
+    var node = document.getElementById('lista_cadastro');
+    var clone = node.cloneNode(true); 
+    document.getElementById('lista_busca').appendChild(clone); 
+
+    while (temp != aux){
+        let lista = document.getElementById("lista_busca");
+
+
+        if(document.getElementById("h-"+temp+"-child-"+child_index)==keyword){
+
+            var node = document.getElementById('lista_cadastro');
+            var clone = node.cloneNode(true); 
+            document.getElementById('tabela_busca').appendChild(clone); 
+
+
+        }
+        child_index++;
+        temp ++;
+    }
+}
 
 function index() {
     let tabela = document.querySelectorAll(".house");
